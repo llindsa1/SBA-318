061c7a564e9a6539823e9e6be89c377d968d6c2d
llindsa1 <lindsayuchiha@icloud.com>
2024-10-15 09:00:10 -0400

    updated server.mjs

--- server.mjs
+++ server.mjs
@@ -1,12 +1,14 @@
 const express = import ('express');
-const mongoose = import ('mongoose');
+const app = express();
 const bodyParser = import('body-parser');
 const methodOverride = import ('method-override');
-const app = express();
+const mongoose = import ('mongoose');
+
 
 //Middleware
 app.use(bodyParser.urlencoded({ extended:true}));
 app.use(methodOverride('_method'));
+app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
 //MongoDB
@@ -17,7 +19,15 @@ mongoose.connect('mongodb://localhost:3000/lindalindsayblog',{
 
 //Routes
 const PORT= process.env.PORT || 3000;
+
+const usersRoute = import('./Routes/users.mjs');
+const postsRoute = import('./Routes/posts.mjs');
+const commentsRoute = import('./Routes/comments.mjs');
+
+app.use('/users', usersRoute);
+app.use('/posts', postsRoute);
+app.use('/comments', commentsRoute);
+
 app.listen(PORT, () => {
     console.log(`Server runningon port ${PORT}`);
 });
-
